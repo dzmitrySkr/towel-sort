9b32eeabbe2423d6032deee4e4cf381445b5837e
Dzmitry <87767424+dzmitrySkr@users.noreply.github.com>
2022-02-05 12:46:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ module.exports = function towelSort (matrix = []) {
   let arr = [];
 
   for (i = 0; i< matrix.length; i++){
-    for (j = 0; j<matrix[i].length; j++){
-      arr.push(matrix[i][j])
+    for (j = 0; j < matrix[i].length; j++){
+
+     i%2 === 0 ? arr.push(matrix[i][j])  : arr.push(matrix[i][matrix[i].length - j -1])
+
     }
   }
 
